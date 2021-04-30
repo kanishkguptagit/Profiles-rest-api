abf5948735fe8bc82d3dd9ade2e3154ca52b8394
kanishkguptagit <1905902@kiit.ac.in>
2021-05-01 03:25:12 +0530

    added new custom user model

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+#For custom user model
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
